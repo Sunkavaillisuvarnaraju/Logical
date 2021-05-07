867f32fafc5b70787fab0e9f55c830163c6fa7f3
SLV SLO <SLV SLO@DESKTOP-A9D5BF7>
2021-05-07 14:28:23 +0530

    Intial Test

---
+++ Practice01/src/com/nit/LogicalPro1/Test01.java
@@ -0,0 +1,14 @@
+package com.nit.LogicalPro1;
+
+public class Test01 {
+
+	public static void main(String[] args) {
+		int a=10;
+		int b=20;
+		int c=a+b;
+		System.out.println("Result:"+c);
+				
+
+	}
+
+}
